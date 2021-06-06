c152d7c72813b5cd62b6a830e39c7f160455c949
Matthew Folbigg <matthew.folbigg@icloud.com>
2021-06-06 14:55:38 +0100

    Initial Commit

---
+++ SetGame-CS193P/SetGame_CS193PApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGame_CS193PApp.swift
+//  SetGame-CS193P
+//
+//  Created by Matthew Folbigg on 06/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SetGame_CS193PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
